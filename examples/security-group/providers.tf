499c98d1faaf86ea1baa845846b61d63ca90b62b
theashishprasad <ashishprasadoffic@gmail.com>
2026-07-31 21:13:41 +0530

    fix: update provider for aws

--- examples/security-group/providers.tf
+++ examples/security-group/providers.tf
@@ -1,14 +1,3 @@
 provider "aws" {
-  access_key                  = "mock"
-  secret_key                  = "mock"
-  region                      = "us-east-1"
-  s3_use_path_style           = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    # The 'ec2' endpoint controls all VPC and Networking components
-    ec2 = "http://localhost:4566"
-  }
+  region = "ap-south-1"
 }
